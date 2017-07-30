d446506e871f7d098e39398287f9ce3609558988
Marcus Loren Lopes Torres <marcusltorres@gmail.com>
2017-07-30 15:51:56 -0300

    Enviando Código

---
+++ frontend/src/template/pageHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default (props) => (
+    
+    
+<header className='page-header'>
+    
+<h2>{props.name} <small>{props.small}</small></h2>
+    
+    </header>
+    
+    
+)
